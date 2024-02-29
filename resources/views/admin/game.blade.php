36365d263123283d5b100e6a7e1927b6157c1f52
baderlt <latrachbadr9@gmail.com>
2024-02-29 21:06:09 +0100

    add from for delete Game

--- resources/views/admin/game.blade.php
+++ resources/views/admin/game.blade.php
@@ -58,7 +58,13 @@
                    <p id='deleted'> Deleted</p>
                   @else 
                   <div style="text-align:right;width: 100%;">
-<button style="text-align: right;" id="delete" > Delete</button> 
+                  <form action="{{ route('admin.games_delete', ['slug' => $game->slug]) }}" method="POST">
+            @csrf
+            @method('DELETE')
+
+<button style="text-align: right;"  data-slug="{{$game->slug}}" id="delete" > Delete</button> 
+    </from>
+
 </div>
                 @endif 
 
@@ -83,12 +89,12 @@
                 <form id="formDelete" action=""  onsubmit="return confirm('Are you sure you want to delete scores ?')">
     @csrf
     @method('DELETE')   
-    <button type="button"  id="Btn-delete" data-game="{{$game->slug}}"  class="Deleted">
+    <button type="submit"  id="Btn-delete" data-game="{{$game->slug}}"  class="Deleted">
            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                     </svg>
-        
+       
                 </button>
                 </form>
             </div>
@@ -120,12 +126,13 @@
     <script  >
 
 const delete_Btn =document.getElementById('Btn-delete')
+console.log(delete_Btn.dataset.game);
 // console.log(delete_Btn);  
 delete_Btn.addEventListener('click',(e)=>{     
     //  console.log(e); 
         const form3 = document.getElementById('formDelete');
         const version = document.getElementById('version').value;
-        // console.log(e.target.dataset.game);
+        console.log(e.target.dataset.game);
         // console.log(form3)
         form3.setAttribute("action","/admin/scores/"+e.target.dataset.game+"?version="+version);
         form3.setAttribute("methode","delete");
