8df9c0b3c68a1c18a9aa0f20864348f9acba1c93
baderlt <baderlatrache10@gmail.com>
2024-03-20 23:15:39 +0000

    otpm

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,9 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 Route::post('/user/register', [AuthController::class, 'createUser']);
 Route::post('/user/login', [AuthController::class, 'loginUser']);
 
